f5d7fcc484d9423e064bdd89b9c1431878594273
Ray <70123218+Rayraygetwild@users.noreply.github.com>
2026-03-01 00:27:37 -0500

    Revert "Revert "Finished NFLPlayerStats layout""

---
+++ Sports_Project_1/NFLPlayerStats.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sports_Project_1
+{
+    public partial class NFLPlayerStats : Form
+    {
+        public int playerID;
+
+        public NFLPlayerStats(int pID, Image logo)//playerid is here so anytime form is opened we can identify what player it is with this
+        {
+            InitializeComponent();
+
+            playerID = pID;
+            pBoxLogo.Image = logo;
+        }
+
+        private void butExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        //labels for player name and team name, textboxes for player info and player stats
+    }
+}
